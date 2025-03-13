fcb7b58a9f8ee72f00aa66ac5611e7f8631f3b00
Abajit Dey <abajitdeyag10118@gmail.com>
2025-03-14 00:02:54 +0530

    Fixed routing on user being active

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,10 @@
 import Button from "./button";
 import { ArrowRight, Radio } from "lucide-react";
 import { NavLink } from "react-router";
+import { auth } from "../../lib/firebase";
 
 export default function Hero() {
+  const user = auth;
   return (
     <section className="w-full min-h-screen flex flex-col items-center justify-center">
       <div className="flex items-center justify-center mb-8">
@@ -21,12 +23,21 @@ export default function Hero() {
           </p>
         </div>
         <div className="flex flex-col gap-2 min-[400px]:flex-row w-full">
-          <NavLink
-            to="/auth/signup"
-            className="flex items-center gap-2 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Get Started <ArrowRight className="h-4 w-4" />
-          </NavLink>
+          {user.currentUser ? (
+            <NavLink
+              to="/app"
+              className="flex items-center gap-2 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Get Started <ArrowRight className="h-4 w-4" />
+            </NavLink>
+          ) : (
+            <NavLink
+              to="/auth/signup"
+              className="flex items-center gap-2 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Get Started <ArrowRight className="h-4 w-4" />
+            </NavLink>
+          )}
           <Button size="lg" variant="outline">
             Learn More
           </Button>
